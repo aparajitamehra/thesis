06a378040af7e70e9b4e134324d6a243c1507af7
Tomas Gareau <tggareau@gmail.com>
2020-02-08 21:09:06 +0100

    Separate fit and transform steps of HighVIFDropper
    Previously, HighVIFDropper was identifying and dropping high VIF columns
    in the `transform` method. However, the identification of those columns
    should occur in the `fit` method instead (otherwise, we'll fit again on
    test data which may have different VIF characteristics).
    
    Also includes some changes to work with np array instead of pandas
    DataFrame: sklearn Pipeline instances will convert pandas DataFrames to
    np arrays when calling the `fit` or `transform` functions of
    HighVIFDropper. Also modified HighVIFDropper

--- utils/preprocessing.py
+++ utils/preprocessing.py
@@ -2,46 +2,46 @@
 
 from sklearn.base import BaseEstimator, TransformerMixin
 from statsmodels.stats.outliers_influence import variance_inflation_factor
+import numpy as np
 
 
 class HighVIFDropper(BaseEstimator, TransformerMixin):
     """Transformer that drops numerical columns with high variance inflation factor."""
 
     def __init__(self, threshold=10):
-        self._threshold = threshold
+        self.threshold = threshold
+        self._high_vif_cols = []
 
     def fit(self, X, y=None):
-        """Does nothing for the HighVIFDropper."""
+        """Identifies columns with a VIF greater than `self.threshold`."""
+        self._identify_high_vif(X)
+        print(f"Dropping columns '{', '.join([str(i) for i in self._high_vif_cols])}'")
         return self
 
     def transform(self, X, y=None):
-        """Drops columns in X with a VIF greater than `self._threshold`."""
-        return self._drop_high_vif(X, self._threshold)
+        """Drops columns identified in the fit method."""
+        return np.delete(X, self._high_vif_cols, axis=1)
 
-    @staticmethod
-    def _drop_high_vif(data, threshold=10):
-        """Drops numerical columns with a variance inflation factor (VIF) over `threshold`.
+    def _identify_high_vif(self, data):
+        """Identifies columns with a variance inflation factor (VIF) over `self.threshold`.
 
         Params:
-            data: a pandas DataFrame
-            threshold: columns with a VIF higher than this value will be dropped
+            data: an np.ndarray of numerical data with shape (num_samples, num_columns)
         """
 
+        original_indices = [i for i in range(data.shape[1])]
         drop = True
         while drop:
             drop = False
-            numeric_cols = data.select_dtypes("number").columns
             max_vif = -1
             max_col = None
-            for i, col in enumerate(numeric_cols):
-                vif = variance_inflation_factor(data[numeric_cols].values, i)
+            for i, col in enumerate(data.T):
+                vif = variance_inflation_factor(data, i)
                 if vif > max_vif:
                     max_vif = vif
-                    max_col = col
+                    max_col = i
 
-            if max_vif > threshold:
-                print(f"Dropping column '{max_col}' with VIF: {max_vif}")
-                data = data.drop([max_col], axis=1)
+            if max_vif > self.threshold:
+                self._high_vif_cols.append(original_indices.pop(max_col))
+                data = np.delete(data, max_col, axis=1)
                 drop = True
-
-        return data
