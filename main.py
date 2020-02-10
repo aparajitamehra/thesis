bc5fb17cd31fd3cbdee164a128a91dec42d002c6
aparajitamehra <aparajitamehra@gmail.com>
2020-02-10 09:36:33 +0100

    Clean up main.py and try other datasets

--- main.py
+++ main.py
@@ -1,42 +1,11 @@
-from sklearn.compose import ColumnTransformer
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.ensemble import AdaBoostClassifier
-from sklearn.impute import SimpleImputer
 from sklearn.linear_model import LogisticRegressionCV
 from sklearn.model_selection import train_test_split
 from sklearn.pipeline import Pipeline
-from sklearn.preprocessing import StandardScaler, OneHotEncoder
 
 from utils.data import load_credit_scoring_data
-from utils.preprocessing import HighVIFDropper
-
-
-def create_preprocessing_pipeline(data):
-    numeric_features = data.select_dtypes("number").columns
-    numeric_pipeline = Pipeline(
-        steps=[
-            ("imputer", SimpleImputer(strategy="median")),
-            ("vif_dropper", HighVIFDropper(threshold=10)),
-            ("scaler", StandardScaler()),
-        ]
-    )
-
-    categorical_features = data.select_dtypes("category").columns
-    categorical_pipeline = Pipeline(
-        steps=[
-            ("imputer", SimpleImputer(strategy="constant", fill_value="missing")),
-            ("onehot", OneHotEncoder(handle_unknown="ignore")),
-        ]
-    )
-
-    preprocessor = ColumnTransformer(
-        transformers=[
-            ("numerical", numeric_pipeline, numeric_features),
-            ("categorical", categorical_pipeline, categorical_features),
-        ]
-    )
-
-    return preprocessor
+from utils.preprocessing import create_preprocessing_pipeline
 
 
 def create_classifier(preprocessor, classifier):
@@ -47,10 +16,10 @@ def create_classifier(preprocessor, classifier):
 
 def main():
     data = load_credit_scoring_data(
-        "datasets/UK/input.txt", "datasets/UK/descriptor.csv"
+        "datasets/bene2/input_bene2.txt", "datasets/bene2/descriptor_bene2.csv"
     )
 
-    y = data.pop("Censor")
+    y = data.pop("censor")
     X = data
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
 
