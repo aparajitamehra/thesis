49e666b6a2013d9662d4d51c56f17a75c758e732
aparajitamehra <aparajitamehra@gmail.com>
2020-02-18 21:38:56 +0100

    Updated entity embedding to be more concise

--- entity_embedding.py
+++ entity_embedding.py
@@ -1,55 +1,30 @@
-import matplotlib.pylab as plt
 import numpy as np
 from numpy import savetxt
+import matplotlib.pylab as plt
+from IPython.display import Image
 
+from sklearn.compose import ColumnTransformer
+from sklearn.impute import SimpleImputer
+from sklearn.model_selection import train_test_split
+from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import StandardScaler
+
 from keras.models import Model
 from keras.layers import Input, Dense, Activation, Reshape
 from keras.layers import Concatenate, Dropout
 from keras.layers.embeddings import Embedding
 from keras.utils import plot_model
-from IPython.display import Image
 
 from utils.data import load_credit_scoring_data
-
-# load data
-data_df = load_credit_scoring_data(
-    "datasets/UK/input_UK.csv", "datasets/UK/descriptor_UK.csv"
-)
-
-# identify target and feature columns
-target = ["censor"]
-features = data_df.columns.difference(["censor"])
-
-# train/val/test split
-X_train, y_train = data_df.iloc[:21000][features], data_df.iloc[:21000][target]
-X_val, y_val = data_df.iloc[21000:27000][features], data_df.iloc[21000:27000][target]
-X_test = data_df.iloc[27000:][features]
-
-# transform numerical variables
-scalar = StandardScaler()
-num_cols = [i for i in X_train.select_dtypes(include=["number"])]
-for i in num_cols:
-    scalar.fit(X_train[i].values.reshape(-1, 1))
-    X_train[i] = scalar.transform(X_train[i].values.reshape(-1, 1))
-    X_val[i] = scalar.transform(X_val[i].values.reshape(-1, 1))
-
-# check missing
-
-
-# categorical embedding cardinality
-embed_cols = [i for i in X_train.select_dtypes(include=["category", "bool"])]
-
-for i in embed_cols:
-    print(i, data_df[i].nunique())
+from utils.preprocessing import HighVIFDropper
 
 
 # categorical features to list
-def preproc(X_train, X_val, X_test, data_df):
+def preproc(X_train, X_val, X_test, data):
     input_list_train = []
     input_list_val = []
     input_list_test = []
-    full_data_list = []
+    input_list_full = []
 
     # the cols to be embedded: rescaling to range [0, # values)
     for c in embed_cols:
@@ -60,48 +35,65 @@ def preproc(X_train, X_val, X_test, data_df):
         input_list_train.append(X_train[c].map(val_map).fillna(0).values)
         input_list_val.append(X_val[c].map(val_map).fillna(0).values)
         input_list_test.append(X_test[c].map(val_map).fillna(0).values)
-        full_data_list.append(data_df[c].map(val_map).fillna(0).values)
+        input_list_full.append(data[c].map(val_map).fillna(0).values)
 
     # the rest of the columns
     other_cols = [c for c in num_cols]
     input_list_train.append(X_train[other_cols].values)
     input_list_val.append(X_val[other_cols].values)
     input_list_test.append(X_test[other_cols].values)
-    full_data_list.append(data_df[other_cols].values)
+    input_list_full.append(data[other_cols].values)
 
-    return input_list_train, input_list_val, input_list_test, full_data_list
+    return input_list_train, input_list_val, input_list_test, input_list_full
 
 
-# rename categorical embeddings and print info
-for categorical_var in embed_cols:
-    cat_emb_name = categorical_var.replace(" ", "") + "_Embedding"
+# load data
+data = load_credit_scoring_data(
+    "datasets/UK/input_UK.csv", "datasets/UK/descriptor_UK.csv"
+)
 
-    no_of_unique_cat = X_train[categorical_var].nunique()
-    embedding_size = int(min(np.ceil((no_of_unique_cat) / 2), 50))
+# identify target and feature columns
+target = ["censor"]
+features = data.columns.difference(["censor"])
 
-    print(
-        "Categorical Variable:",
-        categorical_var,
-        "Unique Categories:",
-        no_of_unique_cat,
-        "Embedding Size:",
-        embedding_size,
-    )
+# train/val/test split
+X_train, X_test, y_train, y_test = train_test_split(
+    data[features], data[target], test_size=0.2
+)
 
-# rename categorical inputs and print
-for categorical_var in embed_cols:
-    input_name = "Input_" + categorical_var.replace(" ", "")
-    print(input_name)
+X_train, X_val, y_train, y_val = train_test_split(X_train, y_train, test_size=0.2)
+
+# transform numerical variables
+numeric_features = X_train.select_dtypes("number").columns
+numeric_pipeline = Pipeline(
+    steps=[
+        ("imputer", SimpleImputer(strategy="median")),
+        ("vif_dropper", HighVIFDropper(threshold=10)),
+        ("scaler", StandardScaler()),
+    ]
+)
+numeric_columns = ColumnTransformer(
+    transformers=[("numerical", numeric_pipeline, numeric_features)]
+)
+num_cols = [i for i in X_train.select_dtypes(include=["number"])]
+for i in num_cols:
+    X_train[i].values.reshape(-1, 1)
+    X_train[i] = X_train[i].values.reshape(-1, 1)
+    X_val[i] = X_val[i].values.reshape(-1, 1)
+
+# check missing
 
 input_models = []
 output_embeddings = []
 
 # build inputs and outputs for categorical
+embed_cols = [i for i in X_train.select_dtypes(include=["category", "bool"])]
+
 for categorical_var in embed_cols:
     cat_emb_name = categorical_var.replace(" ", "") + "_Embedding"
 
     no_of_unique_cat = X_train[categorical_var].nunique()
-    embedding_size = int(min(np.ceil((no_of_unique_cat) / 2), 50))
+    embedding_size = int(min(np.ceil(no_of_unique_cat / 2), 50))
 
     input_model = Input(shape=(1,))
     output_model = Embedding(no_of_unique_cat, embedding_size, name=cat_emb_name)(
@@ -141,7 +133,7 @@ model.summary()
 
 # apply model to data and fit
 X_train_list, X_val_list, X_test_list, full_data_list = preproc(
-    X_train, X_val, X_test, data_df
+    X_train, X_val, X_test, data
 )
 history = model.fit(
     X_train_list,
@@ -161,8 +153,9 @@ plt.xlabel("epoch")
 plt.legend(["train", "val"], loc="upper right")
 plt.show()
 
-# extract weights to data file
 embedding_model = Model(inputs=input_models, outputs=output_embeddings)
+
+# extract predicted weights to data file
 predictions = embedding_model.predict(full_data_list)
 embedded_data = np.concatenate(predictions, axis=1)
 savetxt("embedded_UK.csv", embedded_data, delimiter=",")
